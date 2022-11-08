c5e5bddd282d6e6ba881653b698b680691c8b7c6
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-11-09 02:36:38 +0600

    successfully create a api for showing three component in home page

--- index.js
+++ index.js
@@ -18,13 +18,28 @@ const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clus
 console.log(uri);
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
+async function run(){
+    try{
+        const serviceCollection=client.db('flytographer').collection('services');
+
+        app.get('/threeservices', async(req, res)=>{
+       
+        const query={}
+        const cursor=serviceCollection.find(query).limit(3);
+        const services= await cursor.toArray();
+        res.send(services);
+        console.log(services);
+       })
+
+    }
+    finally{
+
+    }
+   
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('genius car server is running')
